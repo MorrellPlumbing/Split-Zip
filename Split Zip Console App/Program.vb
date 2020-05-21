307200fbb90af4a25854df43f431051a68567c72
dalemorrell <dmorrell@morrellplumbing.com.au>
2020-05-21 16:37:53 +1000

    Original working program

---
+++ Split Zip Console App/Program.vb
@@ -0,0 +1,86 @@
+Imports System.Runtime.CompilerServices
+Imports CommandLine
+
+Module Program
+    Class options
+        <[Option]("f", "files", Required:=False, [Default]:=100, HelpText:="The maximum number of files to be placed in each output zip file.")>
+        Public Property MaxFiles As Integer
+
+        <[Option]("b", "bytes", Required:=False, [Default]:=300000000, HelpText:="The maximum size in bytes iof each output zip file.")>
+        Public Property MaxSize As Integer
+
+        <[Option]("d", "deletesource", Required:=False, HelpText:="True to delete the input file on successfull completion.")>
+        Public Property DeleteSource As Boolean = False
+
+        <[Option]("o", "outputFolder", Required:=False, HelpText:="Output folder for the output zip files. If ommitted it uses the source file's folder.")>
+        Public Property OutputFolder As String = String.Empty
+
+        <[Option]("v", "verbose", Required:=False, HelpText:="Set output to verbose messages.")>
+        Public Property Verbose As Boolean = False
+
+        <Value(0, Required:=True, HelpText:="Files to be split into output zip files.")>
+        Public Property InputFiles As IEnumerable(Of String)
+    End Class
+
+    Private Verbose As Boolean
+    Private Const Hyphens = "---------------------------------------------------------------------------"
+
+    <Extension>
+    Private Sub Echo(ByVal aString As String)
+        If Verbose Then
+            Console.WriteLine(aString)
+        End If
+    End Sub
+
+    Private Function SplitFiles(opts As options) As Integer
+        Dim retVal As Integer = 0
+        Verbose = opts.Verbose
+
+        Console.WriteLine(Hyphens)
+        Echo("Verbose output")
+        Hyphens.Echo
+        String.Format("Maximum files: {0}", opts.MaxFiles).Echo
+        String.Format("Maximum size:  {0:#,##0} bytes", opts.MaxSize).Echo
+        String.Format("Delete source: {0}", opts.DeleteSource.ToString).Echo
+        If String.IsNullOrEmpty(opts.OutputFolder) Then
+            String.Format("Output Folder: {0}", "Same as source file").Echo
+        Else
+            String.Format("Output Folder: {0}", opts.OutputFolder).Echo
+        End If
+        Hyphens.Echo
+        Console.WriteLine("{0} file{1} to process", opts.InputFiles.Count, IIf(opts.InputFiles.Count = 1, "", "s"))
+        Console.WriteLine(Hyphens)
+        For Each fileName In opts.InputFiles
+            Console.WriteLine("Processing {0}", IO.Path.GetFileName(fileName))
+            If IO.File.Exists(fileName) Then
+                Dim rezip = New Split_Zip.SplitZip
+                rezip.MaxZipFiles = opts.MaxFiles
+                rezip.MaxZipBytes = opts.MaxSize
+                rezip.OutputFolder = opts.OutputFolder
+                rezip.SourceFilePath = fileName
+                Dim outputfiles = rezip.DestinationFilePaths
+                If outputfiles(0) = fileName Then
+                    Console.WriteLine("Original file is not a zip file or already meets limits. Use original file.")
+                Else
+                    Console.WriteLine("{0} output file{1} created", outputfiles.Count, IIf(outputfiles.Count = 1, "", "s"))
+                    For Each output In outputfiles
+                        output.Echo
+                    Next
+                    If opts.DeleteSource Then
+                        IO.File.Delete(fileName)
+                    End If
+                End If
+            Else
+                Console.WriteLine("File not found.")
+                retVal += 1
+            End If
+            Console.WriteLine(Hyphens)
+        Next
+        Return retVal
+    End Function
+
+    <STAThread>
+    Sub Main(args As String())
+        Parser.Default.ParseArguments(Of options)(args).WithParsed(Function(opts As options) SplitFiles(opts)).WithNotParsed(Function(errs As IEnumerable(Of [Error])) 1)
+    End Sub
+End Module
---
+++ Split Zip/SplitZip.vb
@@ -0,0 +1,143 @@
+Imports Ionic.Zip
+
+Public Class SplitZip
+    Public Property OutputFolder As String = String.Empty
+
+    Private _maxZipFiles As Integer = 100
+    Public Property MaxZipFiles() As Integer
+        Get
+            Return _maxZipFiles
+        End Get
+        Set(ByVal value As Integer)
+            If value <> _maxZipFiles Then
+                _maxZipFiles = value
+                _destinationFilePaths = Nothing
+                'DeleteTempFiles()
+            End If
+        End Set
+    End Property
+
+    Private _maxZipBytes As Integer = 300000000
+    Public Property MaxZipBytes() As Integer
+        Get
+            Return _maxZipBytes
+        End Get
+        Set(ByVal value As Integer)
+            If value <> _maxZipBytes Then
+                _maxZipBytes = value
+                _destinationFilePaths = Nothing
+                'DeleteTempFiles()
+            End If
+        End Set
+    End Property
+
+    Private _sourceFilePath As String = String.Empty
+    Public Property SourceFilePath() As String
+        Get
+            Return _sourceFilePath
+        End Get
+        Set(ByVal value As String)
+            If _sourceFilePath <> value Then
+                If IO.File.Exists(value) Then
+                    _sourceFilePath = value
+                    _destinationFilePaths = Nothing
+                    If OutputFolder = String.Empty Then
+                        OutputFolder = IO.Path.GetDirectoryName(value)
+                    End If
+                    'DeleteTempFiles()
+                Else
+                    Throw New IO.FileNotFoundException("File not found.", value)
+                End If
+            End If
+        End Set
+    End Property
+
+    Private _destinationFilePaths As List(Of String) = Nothing
+    Public Function DestinationFilePaths() As IEnumerable(Of String)
+        If _destinationFilePaths Is Nothing And IO.File.Exists(_sourceFilePath) Then
+            If ZipFile.IsZipFile(_sourceFilePath) Then
+                Using zip = ZipFile.Read(_sourceFilePath)
+                    Dim zipFile As New IO.FileInfo(_sourceFilePath)
+                    If zipFile.Length <= MaxZipBytes And zip.Count <= MaxZipFiles Then
+                        _destinationFilePaths = New List(Of String) From {_sourceFilePath}
+                    Else
+                        _destinationFilePaths = SplitFile(zip.EntriesSorted.ToList)
+                    End If
+                End Using
+            Else
+                _destinationFilePaths = New List(Of String) From {_sourceFilePath}
+            End If
+        End If
+        Return _destinationFilePaths
+    End Function
+
+    Private Function SplitFile(entries As IList(Of ZipEntry), Optional depth As Integer = 0) As IEnumerable(Of String)
+        Dim i As Integer = 0
+        Dim currentSize As Long = 0
+        Dim retval = New List(Of String)
+        Using source = ZipFile.Read(_sourceFilePath)
+            Using target = New ZipFile()
+                Do
+                    Dim entry = entries(i)
+                    target.AddEntry(entry.FileName, entry.OpenReader)
+                    i += 1
+                    currentSize += entry.CompressedSize
+                Loop Until i = entries.Count - 1 Or target.Count >= MaxZipFiles Or currentSize >= MaxZipBytes
+                IO.Directory.CreateDirectory(OutputFolder)
+                Dim filename = IO.Path.Combine(OutputFolder, IO.Path.GetFileNameWithoutExtension(_sourceFilePath) + "_" + CStr(depth) + ".zip")
+                target.Save(filename)
+                retval.Add(filename)
+                If i < entries.Count - 1 Then
+                    retval.AddRange(SplitFile(entries.Skip(i).ToList, depth + 1))
+                End If
+            End Using
+        End Using
+        Return retval
+    End Function
+
+    Private Sub DeleteTempFiles()
+        If _destinationFilePaths IsNot Nothing Then
+            For Each filepath In _destinationFilePaths
+                If filepath <> _sourceFilePath And IO.File.Exists(filepath) Then
+                    Try
+                        IO.File.Delete(filepath)
+                    Catch
+                    End Try
+                End If
+            Next
+        End If
+    End Sub
+
+    '#Region "IDisposable Support"
+    '    Private disposedValue As Boolean ' To detect redundant calls
+
+    '    ' IDisposable
+    '    Protected Overridable Sub Dispose(disposing As Boolean)
+    '        If Not disposedValue Then
+    '            If disposing Then
+    '                ' TODO: dispose managed state (managed objects).
+    '            End If
+
+    '            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+    '            DeleteTempFiles()
+    '            ' TODO: set large fields to null.
+    '        End If
+    '        disposedValue = True
+    '    End Sub
+
+    '    ' TODO: override Finalize() only if Dispose(disposing As Boolean) above has code to free unmanaged resources.
+    '    Protected Overrides Sub Finalize()
+    '        '    ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '        Dispose(False)
+    '        MyBase.Finalize()
+    '    End Sub
+
+    '    ' This code added by Visual Basic to correctly implement the disposable pattern.
+    '    Public Sub Dispose() Implements IDisposable.Dispose
+    '        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+    '        Dispose(True)
+    '        ' TODO: uncomment the following line if Finalize() is overridden above.
+    '        GC.SuppressFinalize(Me)
+    '    End Sub
+    '#End Region
+End Class
